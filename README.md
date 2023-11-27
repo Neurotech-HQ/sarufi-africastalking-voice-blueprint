29ba88c62e7c906217596f430d6eb2b67dbc7ad4
Kalebu Gwalugano <isaackeinstein@gmail.com>
2023-11-27 23:43:06 +0300

    added voice blueprint

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# sarufi-africastalking-voice-blueprint
-Simplify the integration between AT Voice API and Sarufi
+# Sarufi Africa's Talking Voice Blueprint
+
+This repository contains a FastAPI application for handling voice call requests, specifically designed for integration with Africa's Talking Voice API. The application utilizes the Sarufi library for natural language processing in voice interactions.
+
+## Getting Started
+
+### Prerequisites
+
+1. **Python**: Make sure you have Python installed on your machine. You can download it from [python.org](https://www.python.org/).
+
+2. **Dependencies**: Install the required dependencies by running the following command in the project's root directory:
+
+```bash
+pip install -r requirements.txt
+```
+### Environment Variables
+
+Set up the necessary environment variables. Create a .env file in the root directory of the project and add the following:
+
+```bash 
+SARUFI_API_KEY=your_sarufi_api_key
+SARUFI_BOT_ID=your_sarufi_bot_id
+```
+
+> Replace your_sarufi_api_key and your_sarufi_bot_id with your actual Sarufi API key and bot ID.
+
+### Running the Application
+
+To run the application, execute:
+
+```bash
+uvicorn main:app --reload
+```
+
+This will start the FastAPI server on the default port 8000.
+
+### Usage
+
+Once the server is running, it will be listening for POST requests at the /voicemail endpoint. 
+
+1. sessionId
+2. isActive
+3. dtmfDigits
+
+> Use Ngrok or Replit to deploy your webhook so that it can be available on the internet, ready to start receiving requests. 
+
+### Contributing
+
+Contributions to this project are welcome. Please fork the repository and submit a pull request with your changes.
+
+### License
+
+This project is licensed under the MIT License.
+
---
+++ app.py
@@ -0,0 +1,120 @@
+import os
+from sarufi import Sarufi
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import Response
+
+
+# Load environment variables
+load_dotenv()
+
+# Initialize Sarufi and get the bot
+sarufi = Sarufi(api_key=os.getenv("SARUFI_API_KEY"))
+sarufi_bot = sarufi.get_bot(os.getenv("SARUFI_BOT_ID"))
+
+app = FastAPI()
+
+
+@app.post("/voicemail")
+async def voicemail(request: Request):
+    """
+    Handle incoming voice call requests.
+
+    Parameters:
+    - request: FastAPI Request object containing the incoming call data.
+
+    Returns:
+    - FastAPI Response object with appropriate XML response.
+    """
+    try:
+        form_data = await request.form()
+        session_id = form_data["sessionId"]
+        is_active = int(form_data["isActive"])
+        dtfm_digits = form_data.get("dtmfDigits", None)
+        if dtfm_digits:
+            dtfm_digits = int(dtfm_digits.strip())
+
+        if is_active == 1:
+            # If the call is active, respond with a message and play an audio file
+            sarufi_response = (
+                sarufi_bot.respond("start")
+                if not dtfm_digits
+                else sarufi_bot.respond(dtfm_digits)
+            )
+            s_response = ".".join(sarufi_response.get("message"))
+            next_state = sarufi_response.get("next_state")
+
+            if s_response:
+                sarufi_response = "Please listen to our awesome record"
+                if next_state != "end":
+                    response = VoiceResponse.get_digits(s_response)
+                else:
+                    response = VoiceResponse.say(s_response)
+            else:
+                response = VoiceResponse.say(sarufi_response)
+
+            template = """<?xml version="1.0" encoding="UTF-8"?>
+                        <Response>
+                        {response}
+                        </Response>"""
+            response = template.format(response=response)
+            return Response(content=response, media_type="application/xml")
+
+        else:
+            # If the call is not active, read in call details
+            duration = form_data["durationInSeconds"]
+            currency_code = form_data["currencyCode"]
+            amount = form_data["amount"]
+            print("Call duration is {} seconds".format(duration))
+            print("The currency code is {}".format(currency_code))
+            print("The amount received is {}".format(amount))
+            print("The session ID is {}".format(session_id))
+            print("The call has ended")
+
+            # You can store this information in the database for your records
+            return ""
+    except Exception as e:
+        # Log any unexpected exceptions and return an error response
+        print(f"Error: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+
+
+class VoiceResponse:
+    """
+    Class to handle voice response.
+    """
+
+    @staticmethod
+    def say(response):
+        return f"<Say>{response}</Say>"
+
+    @staticmethod
+    def play(response):
+        return f"<Play>{response}</Play>"
+
+    @staticmethod
+    def get_digits(response, timeout=20, finishOnKey="#"):
+        return f"""
+                <GetDigits timeout="{timeout}" finishOnKey="{finishOnKey}">
+                    <Say>{response}</Say>
+                </GetDigits>
+                """
+
+    @staticmethod
+    def partial_record(
+        response, trim_silence=True, play_beep=True, finishOnKey="#", maxLength=10
+    ):
+        return f"""
+                <Record finishOnKey="{finishOnKey}" maxLength="{maxLength}" trimSilence="{trim_silence}" playBeep="{play_beep}">
+                    <Say>{response}</Say>
+                </Record>
+                """
+
+    @staticmethod
+    def terminal_record(response, play_beep=True):
+        return f"""
+                <Say playBeep="{play_beep}">{response}</Say>
+                <Record />
+                """
